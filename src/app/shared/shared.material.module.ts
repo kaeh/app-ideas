46bbb13f4303bc3a98994850379a43115545bec9
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-04-30 14:02:56 +0200

    styles(): Add angular material

--- src/app/shared/shared.material.module.ts
+++ src/app/shared/shared.material.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-@NgModule({})
+@NgModule({
+  exports: [BrowserAnimationsModule],
+})
 export class SharedMaterialModule {}
