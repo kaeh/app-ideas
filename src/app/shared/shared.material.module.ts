07126d40eb7dc22a8c3aa09ca94f250e7051f8a4
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-01 00:18:14 +0200

    style(menu): Add expansion panel for description

--- src/app/shared/shared.material.module.ts
+++ src/app/shared/shared.material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  exports: [BrowserAnimationsModule, MatCardModule, MatIconModule, MatMenuModule, MatButtonModule],
+  exports: [BrowserAnimationsModule, MatCardModule, MatIconModule, MatMenuModule, MatButtonModule, MatExpansionModule],
 })
 export class SharedMaterialModule {}
