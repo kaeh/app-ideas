73a76d33a312037a70731fa3488c57f8fd953617
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-01 17:14:04 +0200

    chore(border-radius-previewer): Change directory structure

--- src/app/features/beginner/border-radius-previewer/border-radius-previewer-routing.module.ts
+++ src/app/features/beginner/border-radius-previewer/border-radius-previewer-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BorderRadiusPreviewerComponent } from './border-radius-previewer/border-radius-previewer.component';
+import { BorderRadiusPreviewerComponent } from './components/border-radius-previewer/border-radius-previewer.component';
 
 const routes: Routes = [{ path: '', component: BorderRadiusPreviewerComponent }];
 
--- src/app/features/beginner/border-radius-previewer/border-radius-previewer.module.ts
+++ src/app/features/beginner/border-radius-previewer/border-radius-previewer.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BorderRadiusPreviewerRoutingModule } from './border-radius-previewer-routing.module';
-import { BorderRadiusPreviewerComponent } from './border-radius-previewer/border-radius-previewer.component';
+import { BorderRadiusPreviewerComponent } from './components/border-radius-previewer/border-radius-previewer.component';
 
 @NgModule({
   declarations: [BorderRadiusPreviewerComponent],
