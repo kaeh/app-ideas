e95e51ae55eb9cccfdbd83233275f3e12068410d
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-01 15:03:23 +0200

    chore(lint): Fix issue

--- src/hmr.ts
+++ src/hmr.ts
@@ -3,10 +3,7 @@
 import { ApplicationRef, NgModuleRef } from '@angular/core';
 import { createNewHosts } from '@angularclass/hmr';
 
-export const hmrBootstrap = (
-  module: any,
-  bootstrap: () => Promise<NgModuleRef<any>>
-) => {
+export const hmrBootstrap = (module: any, bootstrap: () => Promise<NgModuleRef<any>>) => {
   let ngModule: NgModuleRef<any>;
   module.hot.accept();
   bootstrap().then((mod) => (ngModule = mod));
