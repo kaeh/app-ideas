b268019d3ea9459c74dbf38118c5fd0639e2c26e
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-05 19:20:06 +0200

    feature(calculator): Create first few input to allow testing

--- src/app/features/beginner/calculator/calculator.module.ts
+++ src/app/features/beginner/calculator/calculator.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@kaeh/shared/shared.module';
 import { CalculatorRoutingModule } from './calculator-routing.module';
 import { CalculatorComponent } from './components/calculator.component';
 
 @NgModule({
   declarations: [CalculatorComponent],
-  imports: [CalculatorRoutingModule],
+  imports: [CalculatorRoutingModule, SharedModule],
 })
 export class CalculatorModule {}
--- src/app/features/beginner/calculator/components/calculator.component.ts
+++ src/app/features/beginner/calculator/components/calculator.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Operator } from '@kaeh/shared/enums';
+import { evaluateOperation } from '@kaeh/shared/functions';
+import { OperationElement } from '@kaeh/shared/types';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { filter, map, skip, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'kaeh-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss'],
 })
-export class CalculatorComponent {}
+export class CalculatorComponent implements OnInit, OnDestroy {
+  public currentOperation$: Observable<string>;
+  public operationResult$: Observable<string>;
+  public Operator = Operator;
+
+  private _currentOperationSubject = new BehaviorSubject<string>('');
+  private _destroy$ = new Subject<void>();
+
+  public ngOnInit(): void {
+    this.currentOperation$ = this._currentOperationSubject.asObservable().pipe(takeUntil(this._destroy$));
+    this.operationResult$ = this._currentOperationSubject.asObservable().pipe(
+      skip(1), // Skip first as it show 0 instead
+      map(evaluateOperation),
+      filter((r) => !isNaN(r)),
+      map((r) => r?.toString() ?? ''),
+      takeUntil(this._destroy$)
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.unsubscribe();
+    this._currentOperationSubject.unsubscribe();
+  }
+
+  public addToOperation(element: OperationElement): void {
+    let currentOperation = this._currentOperationSubject.getValue();
+    const lastDisplayedElement = currentOperation.slice(-1);
+    if (typeof element === 'string' && isNaN(+lastDisplayedElement)) {
+      return;
+    }
+
+    currentOperation = this._currentOperationSubject.getValue() + element;
+    this._currentOperationSubject.next(currentOperation);
+  }
+}
