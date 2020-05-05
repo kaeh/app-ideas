3f8543a21fecd53aeca55dce616951f9ced8e2fa
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-06 00:28:11 +0200

    chore(jest): Move config to package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
-
-module.exports = {
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-};
