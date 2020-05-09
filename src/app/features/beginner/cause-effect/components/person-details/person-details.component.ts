fd85a3214485ddc11e7d14e15309f94585bedced
Corentin GROSDEMOUGE <corentin.grosdemouge@gmail.com>
2020-05-09 17:07:12 +0200

    feature(cause-effect): Finish feature

--- src/app/features/beginner/cause-effect/components/person-details/person-details.component.ts
+++ src/app/features/beginner/cause-effect/components/person-details/person-details.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Person } from '@kaeh/cause-effect/interfaces';
+import { PersonStateService } from '@kaeh/cause-effect/state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'kaeh-person-details',
@@ -6,4 +9,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./person-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PersonDetailsComponent {}
+export class PersonDetailsComponent implements OnInit {
+  public person$: Observable<Person>;
+  public constructor(private readonly _personStateService: PersonStateService) {}
+
+  public ngOnInit(): void {
+    this.person$ = this._personStateService.selectedPersonChange$;
+  }
+}
